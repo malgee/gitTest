21136378e01389b8b63ec07df41a39020b378aa8
malgee <malgee@163.com>
2018-03-01 15:33:44 +0800

    添加新文件

---
+++ MGTest/MGTest/test.h
@@ -0,0 +1,13 @@
+//
+//  test.h
+//  MGTest
+//
+//  Created by apple on 2018/3/1.
+//  Copyright © 2018年 apple. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface test : NSObject
+
+@end
---
+++ MGTest/MGTest/test.m
@@ -0,0 +1,13 @@
+//
+//  test.m
+//  MGTest
+//
+//  Created by apple on 2018/3/1.
+//  Copyright © 2018年 apple. All rights reserved.
+//
+
+#import "test.h"
+
+@implementation test
+
+@end
